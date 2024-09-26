5e8166411477b0c6b7f2677b45efa2db7e35ef02
Candy <dltkr0424@gmail.com>
2024-09-26 15:57:23 +0900

    SkillSOAndSkillManagerComplete

--- Assets/Work/CD/3. Scripts/Manager/SkillManager.cs
+++ Assets/Work/CD/3. Scripts/Manager/SkillManager.cs
@@ -1,18 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SkillType
+{
+    Dash
+}
+
+
 public class SkillManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Dictionary<Type, Skill> Skills;
+
+    private Player _player;
+
+    private void Awake()
     {
-        
+        Skills = new Dictionary<Type, Skill>();
+    }
+    
+
+    private void Start()
+    {
+        _player = GameObject.Find("Player").GetComponent<Player>();
+        foreach (SkillType skillType in Enum.GetValues(typeof(SkillType)))
+        {
+            Skill skillCompo = GetComponent($"{skillType.ToString()}Skill") as Skill;
+            if (skillCompo != null)
+            {
+                skillCompo.Initialize(_player);
+                
+                Type type = skillCompo.GetType();
+                Debug.Log(type);
+                Debug.Log(skillCompo);
+                
+                Skills.Add(type, skillCompo);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private T GetSkill<T>() where T : Skill
     {
-        
+        Type t = typeof(T);
+        if (Skills.TryGetValue(t, out var targetSkill)) // 해당 Key가 있으면 True
+        {
+            return targetSkill as T;
+        }
+
+        return null;
     }
 }
--- Assets/Work/CD/3. Scripts/SO/SkillSO.cs
+++ Assets/Work/CD/3. Scripts/SO/SkillSO.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SkillSO : MonoBehaviour
+[CreateAssetMenu(menuName = "SO/Skill")]
+public class SkillSO : ScriptableObject
 {
     [Header("SkillName")]
     public string skillName;
