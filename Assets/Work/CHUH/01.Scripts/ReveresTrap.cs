e70c9928d84de27e2458cd5f9bfc84e31578138b
chuh007 <chhuh007@gmail.com>
2024-10-14 00:50:47 +0900

    chore ReveresTrap

--- Assets/Work/CHUH/01.Scripts/ReveresTrap.cs
+++ Assets/Work/CHUH/01.Scripts/ReveresTrap.cs
@@ -1,16 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ReveresTrap : MonoBehaviour
 {
-
-    private void Reveres()
-    {
-
-    }
+    [SerializeField] private float waitTime = 1f;
+    private bool isFilp = false;
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(Reveres());
+        }
     }
+
+    private IEnumerator Reveres()
+    {
+        yield return new WaitForSeconds(waitTime);
+        ReveresGround(isFilp);
+    }
+
+    private void ReveresGround(bool filp)
+    {
+        isFilp = !filp;
+        if(isFilp == true)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+    
 }
