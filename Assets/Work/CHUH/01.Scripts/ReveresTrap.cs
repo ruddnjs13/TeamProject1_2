f305d6ce028990db898982927a3a5b0f0723b9c9
chuh007 <chhuh007@gmail.com>
2024-10-16 01:07:49 +0900

    feat RotatePlatform

--- Assets/Work/CHUH/01.Scripts/ReveresTrap.cs
+++ Assets/Work/CHUH/01.Scripts/ReveresTrap.cs
@@ -6,32 +6,43 @@ using DG.Tweening;
 public class ReveresTrap : MonoBehaviour
 {
     [SerializeField] private float waitTime = 1f;
+    [SerializeField] private float shakeTime = 0.5f;
+    [SerializeField] private float returnTime = 3f;
     private bool isFilp = false;
-    private void OnCollisionEnter2D(Collision2D collision)
+    private bool isCol = false;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player")&&!isCol)
         {
             StartCoroutine(Reveres());
         }
     }
-
     private IEnumerator Reveres()
     {
+        isCol = true;
         yield return new WaitForSeconds(waitTime);
+        transform.DOShakePosition(waitTime, new Vector3(0.05f, 0, 0), 15, 50f, false);
+        yield return new WaitForSeconds(shakeTime);
         ReveresGround(isFilp);
+        yield return new WaitForSeconds(returnTime);
+        ReveresGround(isFilp);
+        isCol = false;
     }
 
     private void ReveresGround(bool filp)
     {
-        isFilp = !filp;
-        if(isFilp == true)
+        
+        if(isFilp == false)
         {
-            
+            transform.DORotate(new Vector3(0,0,180f),0.2f,RotateMode.Fast);
         }
         else
         {
+            transform.DORotate(new Vector3(0, 0, 0), 0.2f, RotateMode.Fast);
 
         }
+        isFilp = !filp;
+
     }
-    
+
 }
