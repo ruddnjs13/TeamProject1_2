980f010c0eb7f23fde77acc79bd8da694c2b2276
chuh007 <chhuh007@gmail.com>
2024-10-16 18:46:04 +0900

    chore

---
+++ Assets/Work/CHUH/01.Scripts/PlayerTrapHit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrapHit : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("µÚ1Áü");
+    }
+}
