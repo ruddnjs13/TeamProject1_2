bb6f095a962be76341940eed8a5299f27104836c
ruddnjs13 <ruddnjs013@gmail.com>
2024-10-18 12:22:18 +0900

    feat PlayerInteracton and Interactable and MapRotate

--- Assets/Work/LKW/01.Scripts/RotateMap.cs
+++ Assets/Work/LKW/01.Scripts/RotateMap.cs
@@ -6,36 +6,50 @@ public class RotateMap : MonoBehaviour,IInteractable
 {
     [SerializeField] private GameObject _playerAxis;
     [SerializeField] private float time;
-
     [SerializeField] private GameObject _map;
+    private PlayerInteraction _playerInteraction;
     private bool isRotate = false;
+    private bool _readyUse = false;
 
-    public void MapRotate()
+    private void Awake()
+    {
+        _playerInteraction = GameObject.Find("Player").GetComponentInChildren<PlayerInteraction>();
+    }
+
+    private void Update()
+    {
+        if (!_readyUse)
+        {
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            MapRotate(1);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            MapRotate(-1);
+        }
+    }
+
+    public void MapRotate(float directionX)
     {
         StopAllCoroutines();
         Debug.Log("회전");
         isRotate = true;
         Physics2D.gravity = new Vector2(0, 0);
         _map.transform.SetParent(_playerAxis.transform);
-        StartCoroutine(MapRotateCoroutine());
+        StartCoroutine(MapRotateCoroutine(directionX));
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            MapRotate();
-        }
-    }
-
-    private IEnumerator MapRotateCoroutine()
+    private IEnumerator MapRotateCoroutine(float directionX)
     {
         float start = 0;
         float current = 0;
         float percent = 0;
 
         Vector3 _axisAngle = _playerAxis.transform.eulerAngles;
-        float target = 90;
+        float target = 90 * directionX;
 
         while (percent < 1)
         {
@@ -43,22 +57,23 @@ public class RotateMap : MonoBehaviour,IInteractable
             percent = current / time;
 
             _playerAxis.transform.rotation = Quaternion.Euler
-                (new Vector3(_axisAngle.x,_axisAngle.y,_axisAngle.z - Mathf.Lerp(start,target,percent)));
+                (new Vector3(_axisAngle.x,_axisAngle.y,_axisAngle.z + (Mathf.Lerp(start,target,percent) * directionX)));
             yield return null;
         }
 
         Physics2D.gravity = new Vector2(0, -9.81f);
         _map.transform.SetParent(null);
         isRotate = false;
+        _readyUse = false;
     }
 
-    public string ShowInteratText()
+    public void ShowInteractText()
     {
-        throw new NotImplementedException();
     }
 
     public void Interact()
     {
-        throw new NotImplementedException();
+        Debug.Log("상호작용가능");
+        _readyUse = true;
     }
 }
