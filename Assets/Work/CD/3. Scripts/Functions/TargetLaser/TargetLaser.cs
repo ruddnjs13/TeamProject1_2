4eb4664f56ee06724f6883e6bfa7d5895e8787ae
Candy <dltkr0424@gmail.com>
2024-10-19 18:58:03 +0900

    fix : TargetLaser

--- Assets/Work/CD/3. Scripts/Functions/TargetLaser/TargetLaser.cs
+++ Assets/Work/CD/3. Scripts/Functions/TargetLaser/TargetLaser.cs
@@ -7,6 +7,10 @@ using DG.Tweening;
 
 public class TargetLaser : MonoBehaviour
 {
+    [SerializeField] private LayerMask _whatIsPlayer;
+    
+    [SerializeField] private float _overlapSize;
+    
     [SerializeField] private float _laserWidth;
 
     [SerializeField] private float _laserCool;
@@ -26,6 +30,8 @@ public class TargetLaser : MonoBehaviour
 
     private RaycastHit2D _hit;
 
+    private Collider2D _collider;
+    
     private void Awake()
     {
         _lineRenderer = GetComponent<LineRenderer>();
@@ -49,7 +55,17 @@ public class TargetLaser : MonoBehaviour
 
     private void FixedUpdate()
     {
-        TargetRay();
+        TargetFind();
+    }
+
+    private void TargetFind()
+    {
+        _collider = Physics2D.OverlapCircle(transform.position, _overlapSize, _whatIsPlayer);
+
+        if (_collider != null)
+        {
+            TargetRay();
+        }
     }
 
     private IEnumerator DelayLaser()
@@ -66,22 +82,23 @@ public class TargetLaser : MonoBehaviour
     private void TargetRay()
     {
         _hit = Physics2D.Raycast(transform.position, _dir);
-
+        
         if (_hit)
         {
             _lineRenderer.SetPosition(1, _hit.point);
-            TestDotLaser();
+            _laserSequence.Play();
+            
         }
     }
 
     private void TestDotLaser()
-    {
+    { 
         _laserTween = DOTween.To(() => _lineRenderer.widthMultiplier, x => _lineRenderer.widthMultiplier = x, _laserWidth, _laserSpeed);
         // _laserTween = DOVirtual.Float(0.1f, _laserWidth, _laserSpeed, x => Debug.Log(x));
         _laserSequence = DOTween.Sequence();
-        _laserSequence.AppendInterval(_laserCool / 2);
-        _laserSequence.Append(_laserTween); // 두께가 굵어 지는거.
-        _laserSequence.AppendInterval(_laserCool / 2);
+        _laserSequence.AppendInterval(_laserCool);
+        _laserSequence.Append(_laserTween);
+        _laserSequence.AppendInterval(_laserCool);
         _laserSequence.SetLoops(2, LoopType.Yoyo);
     }
 
@@ -100,8 +117,12 @@ public class TargetLaser : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-
         Gizmos.DrawRay(transform.position, _hit.point - (Vector2)transform.position);
+        Gizmos.color = Color.white;
+        
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, _overlapSize);
+        Gizmos.color = Color.white;
     }
 
 #endif
