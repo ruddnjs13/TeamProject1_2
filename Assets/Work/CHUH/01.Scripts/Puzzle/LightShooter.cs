6e70dccb5e34dd7c243f9f7afe1f0d501fa353ec
chuh007 <chhuh007@gmail.com>
2024-10-22 01:37:22 +0900

    fix Mirror Puzzle

--- Assets/Work/CHUH/01.Scripts/Puzzle/LightShooter.cs
+++ Assets/Work/CHUH/01.Scripts/Puzzle/LightShooter.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
-public class LightShooter : MonoBehaviour
+public class LightShooter : MonoBehaviour, IInteractable
 {
+    private bool isCanLightShoot = true;
     private LineRenderer _lineRenderer;
     private int positionCount = 1;
     private void Awake()
@@ -13,21 +14,41 @@ public class LightShooter : MonoBehaviour
     }
     private void Start()
     {
-        _lineRenderer.SetPosition(0, transform.position);
-        ShootLight();
+        _lineRenderer.SetPosition(0, Vector3.zero); // 최초의 점 위치 자신으로(local 계 쓰니깐 이거임)
     }
-    private void ShootLight()
+    private void FireLight()
     {
-        Debug.Log("슈웃");
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 20);
-        Debug.DrawRay(transform.position, Vector2.up);
-        if (hit.collider != null&&hit.transform.CompareTag("Mirror"))
+        if (!isCanLightShoot) return;
+        Debug.Log("빛이 있으라");
+        StartCoroutine(LightColtime());
+        _lineRenderer.SetPosition(0, Vector3.zero); // 최초의 점 위치 자신으로(local 계 쓰니깐 이거임)
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up, 20); // 레이 쏴서 거룰 있나 체크
+        if (hit.collider != null&&hit.transform.CompareTag("Mirror")) // 닿은놈이 거울이면
         {
-            Debug.Log("거울에맞음");
-            Debug.Log(hit.transform.position);
-            _lineRenderer.positionCount = ++positionCount;
-            _lineRenderer.SetPosition(1, hit.transform.position);
-            hit.transform.GetComponent<MirrorReflection>().ReflectionLight(_lineRenderer,Vector2.up);
+            _lineRenderer.positionCount = positionCount + 1; // lineRander 점 개수 1개 늘리기
+            _lineRenderer.SetPosition(1,transform.InverseTransformPoint(hit.transform.position)); 
+            // 다음 점을 맞은 거울의 위치로 설정(로컬로 전환한 이유는 LineRander의 그려지는걸 로컬로 설정해서)
+            hit.transform.GetComponent<MirrorReflection>().ReflectionLight(_lineRenderer,Vector2.up,this);
+            // 맞은 거울의 반사 스크립트 실행(라인렌더러, 들어가는 방향, 이 오브젝트(local 포지션 계산 위함))
         }
+    }
+
+    private IEnumerator LightColtime()
+    {
+        isCanLightShoot = false;
+        yield return new WaitForSeconds(1);
+        _lineRenderer.positionCount = 1;
+        yield return new WaitForSeconds(3);
+        isCanLightShoot = true;
+    }
+
+    public void ShowInteractText()
+    {
+
+    }
+
+    public void Interact()
+    {
+        FireLight();
     }
 }
--- Assets/Work/CHUH/01.Scripts/Puzzle/MirrorReflection.cs
+++ Assets/Work/CHUH/01.Scripts/Puzzle/MirrorReflection.cs
@@ -7,34 +7,29 @@ public class MirrorReflection : MonoBehaviour
     Ray ray;
     Ray ray2;
     private BoxCollider2D col;
+    private bool isCanTeflection = true;
     // Vector.right가 법선벡터
     private void Awake()
     {
         col = GetComponent<BoxCollider2D>();
     }
-    private void Start()
+    
+    public void ReflectionLight(LineRenderer line,Vector2 dir, LightShooter shooter) // 반복되어 호출되는 거울 반사
     {
-        ray = new Ray(transform.position, transform.right);
-        Debug.DrawRay(transform.position, Vector3.up * 5);
-    }
-    public void ReflectionLight(LineRenderer line,Vector2 dir)
-    {
-        Vector2 reflectionVector = Vector2.Reflect(dir, transform.right);
-        Debug.Log(reflectionVector);
-        col.enabled = false;
+        col.enabled = false; // 이 거울은 맞았으니 다시 반응 안하게 해둠(안해두면 마주보는 거울에서 무한호출)
+        StartCoroutine(LightColtime());
+        Vector2 reflectionVector = Vector2.Reflect(dir, transform.right); // 입사각과 법선벡터로 반사각 구함.
         RaycastHit2D hit = Physics2D.Raycast(transform.position, reflectionVector,20);
-        ray2 = new Ray(transform.position, reflectionVector * 20);
-        int count = line.positionCount;
-        line.positionCount++;
-        if (hit.collider != null)
+        // Shooter처럼 Ray 쏴서 검사
+        if (hit.collider != null) // 조건
         {
-            line.SetPosition(count, hit.transform.position);
-            if (hit.transform.CompareTag("Mirror"))
+            int count = line.positionCount;
+            line.positionCount++;
+            // 점 개수 늘리기(거울에 안맞더라도 맞은데에 마지막 점 그려야 하니깐)
+            line.SetPosition(count, shooter.transform.InverseTransformPoint(hit.transform.position)); // 맞은데에 점 찍기
+            if (hit.transform.CompareTag("Mirror")) // 거울에 맞은거임?
             {
-                Debug.Log(count);
-                Debug.Log("거울에또맞음");
-                Debug.Log(hit.transform.position);
-                hit.transform.GetComponent<MirrorReflection>().ReflectionLight(line, reflectionVector);
+                hit.transform.GetComponent<MirrorReflection>().ReflectionLight(line, reflectionVector,shooter); // 그러면 다시 반사
             }
             else
             {
@@ -42,11 +37,13 @@ public class MirrorReflection : MonoBehaviour
             }
         }
     }
-
-    private void OnDrawGizmos()
+    private IEnumerator LightColtime()
     {
-        Gizmos.color = Color.green;
-        Gizmos.DrawRay(ray);
-        Gizmos.DrawRay(ray2);
+        isCanTeflection = false;
+        yield return new WaitForSeconds(1);
+        col.enabled = true;
+        yield return new WaitForSeconds(3);
+        isCanTeflection = true;
     }
+    
 }
