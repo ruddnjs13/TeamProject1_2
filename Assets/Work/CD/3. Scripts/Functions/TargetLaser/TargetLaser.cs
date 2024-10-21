7dd15d7c2c068eb49f064f1e7bb7f89a81a70e8a
Candy <dltkr0424@gmail.com>
2024-10-22 01:50:50 +0900

    fix : TargetLaser

--- Assets/Work/CD/3. Scripts/Functions/TargetLaser/TargetLaser.cs
+++ Assets/Work/CD/3. Scripts/Functions/TargetLaser/TargetLaser.cs
@@ -7,27 +7,107 @@ using DG.Tweening;
 
 public class TargetLaser : MonoBehaviour
 {
+    [SerializeField] private float _rotationCool;
+    [SerializeField] private float _rotationDuration;
+
+    [SerializeField] private float _size;
+    [SerializeField] LayerMask _whatIsPlayer;
+
     [SerializeField] private Transform _target;
 
     [Range(0, 1f)] [SerializeField] private float _rotateSpeed;
-
+    
     private Vector3 _dir;
 
-    private RaycastHit2D _hit;
+    private RaycastHit2D _ray;
 
     private Collider2D _collider;
+    private LineRenderer _line;
+
+    private Sequence _sequence;
+
+    private void Awake()
+    {
+        _line = GetComponent<LineRenderer>();
+        _line.SetPosition(0, transform.position);
+    }
 
     private void Update()
     {
-        TargetRotateObj();
+        ShootRay();
+        TargetFind();
     }
-    
-    private void TargetRotateObj()
+
+    public void TargetFind()
     {
-        _dir = _target.position - transform.position;
+        _collider = Physics2D.OverlapCircle(transform.position, _size, _whatIsPlayer);
+        
+        if (_collider != null)
+        {
+            _sequence.Kill();
+            Debug.Log("아");
+            if (!_line.enabled) _line.enabled = true;
+            TargetRotateObj(_collider.transform);
+        }
+        else
+        {
+            DefaultRotate();
+        }
+    }
+
+    private void DefaultAngle()
+    {
+        transform.localRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 0), _rotateSpeed);
+    }
+
+    private void DefaultRotate()
+    {
+        if (!_sequence.IsActive())
+            DefaultTween();
+    }
+
+    private void DefaultTween()
+    {
+        _sequence = DOTween.Sequence();
+        _sequence.AppendInterval(_rotationCool)
+            .Append(transform.DORotate(new Vector3(0, 0, -180), _rotationDuration, RotateMode.Fast).SetRelative())
+            .AppendInterval(_rotationCool)
+            .SetLoops(2, LoopType.Yoyo);
+    }
+
+    private void ShootRay()
+    {
+        _ray = Physics2D.Raycast(transform.position, transform.right);
+
+        if (_ray)
+        {
+            _line.SetPosition(1, _ray.point);
+        }
+    }
+
+    private void TargetRotateObj(Transform target = null)
+    {
+        if (!target) return;
+
+        _dir = target.position - transform.position;
 
         float angle = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
 
-        transform.localRotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), _rotateSpeed);
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, 0, angle), _rotateSpeed);
     }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawWireSphere(transform.position, _size);
+        Gizmos.color = Color.white;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.right);
+    }
+
+#endif
 }
