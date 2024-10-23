8cebb97d7ed3a1006d702ebaddb4f33bf6e4c66a
chuh007 <chhuh007@gmail.com>
2024-10-23 19:13:27 +0900

    feat map, fix LightPuzzle, fix ReveresGround, add Tag

--- Assets/Work/CHUH/01.Scripts/Puzzle/MirrorReflection.cs
+++ Assets/Work/CHUH/01.Scripts/Puzzle/MirrorReflection.cs
@@ -30,13 +30,14 @@ public class MirrorReflection : MonoBehaviour, IInteractable
         // Shooter처럼 Ray 쏴서 검사
         int count = line.positionCount;
         line.positionCount++;
+        // 점 개수 늘리기(거울에 안맞더라도 맞은데에 마지막 점 그려야 하니깐)
         if (hit.collider != null) // 조건
         {
-            // 점 개수 늘리기(거울에 안맞더라도 맞은데에 마지막 점 그려야 하니깐)
             line.SetPosition(count, shooter.transform.InverseTransformPoint(hit.point)); // 맞은데에 점 찍기
             if (hit.transform.CompareTag("Mirror")) // 거울에 맞은거임?
             {
-                hit.transform.GetComponent<MirrorReflection>().ReflectionMirror(line, reflectionVector, shooter); // 그러면 다시 반사
+                hit.transform.GetComponent<MirrorReflection>()?.ReflectionMirror(line, reflectionVector, shooter); // 그러면 다시 반사
+                hit.transform.GetComponent<LightSensor>()?.ExecutionEvent();
             }
         }
         else
@@ -63,7 +64,6 @@ public class MirrorReflection : MonoBehaviour, IInteractable
         sequence.Append(transform.DORotate(transform.rotation.eulerAngles + new Vector3(0, 0, 45f), 0.5f));
         sequence.OnComplete(() =>
         {
-            Debug.Log("시퀀스끝남");
             isCanRotate = true;
             col.enabled = true;
         });
