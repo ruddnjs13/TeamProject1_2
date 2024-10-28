b8943f40c7bbc20c04916c9eefdfb986c0199e8b
chuh007 <chhuh007@gmail.com>
2024-10-29 01:13:09 +0900

    Rotate

---
+++ Assets/Work/LKW/01.Scripts/Gimmick/RotateManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RotateManager : MonoSingleton<RotateManager>
+{
+    
+    
+    [SerializeField] private RotateMap1[] _rotateMap1s;
+    [SerializeField] private RotateMap2[] _rotateMap2s;
+    [SerializeField] private Transform _playerTrm;
+    [SerializeField] private Transform _rotateAxis;
+    [SerializeField] private GameObject _grid;
+    [SerializeField] private float _rotateTime1;
+    [SerializeField] private float _rotateTime2;
+
+    public float[] _rotations1 = { 0,90,180,270 };
+
+    public int CurrentRotationIdx { get; set; } = 0;
+
+    private void Awake()
+    {
+        InitializeRotateMaps();
+    }
+
+    private void InitializeRotateMaps()
+    {
+        foreach (RotateMap1 item in _rotateMap1s)
+        {
+            item.Initialize(_playerTrm,_rotateAxis,_grid,_rotateTime1);
+        }
+
+        foreach (RotateMap2 item in _rotateMap2s)
+        {
+            item.Initialize(_playerTrm,_rotateAxis,_grid,_rotateTime2);
+        }
+    }
+}
---
+++ Assets/Work/LKW/01.Scripts/Gimmick/RotateMap1.cs
@@ -0,0 +1,134 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class RotateMap1 : MonoBehaviour,IInteractable
+{
+    private Transform _playerTrm;
+    private float _time;
+    private GameObject _grid;
+    private Transform _rotateAxis;
+    private GameObject _showDirection;
+    
+    private PlayerInteraction _playerInteraction;
+    private bool isRotate = false;
+    [FormerlySerializedAs("_isInteract")] public bool _canRotate = false;
+
+    private Quaternion _leftRot, _rightRot;
+
+    private void Awake()
+    {
+        _showDirection = transform.Find("ShowDirection").gameObject;
+    }
+
+    private void Start()
+    {
+        _leftRot = Quaternion.Euler(0, 0, -90f);
+        _rightRot = Quaternion.Euler(0, 0, 90f);
+    }
+
+    private void Update()
+    {
+        ChoosDirectionAndStart();
+    }
+
+    private void ChoosDirectionAndStart()
+    {
+        if (!_canRotate)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            RotateManager.Instance.CurrentRotationIdx = ++RotateManager.Instance.CurrentRotationIdx % 4;
+
+            
+            MapRotate(_leftRot);
+        }
+        else if (Input.GetKeyUp(KeyCode.E))
+        {
+            RotateManager.Instance.CurrentRotationIdx = --RotateManager.Instance.CurrentRotationIdx;
+            if (RotateManager.Instance.CurrentRotationIdx < 0)
+            {
+                RotateManager.Instance.CurrentRotationIdx = 3;
+            }
+            MapRotate(_rightRot);
+        }
+    }
+
+    public void MapRotate(Quaternion direction)
+    {
+        if (isRotate) return;
+        
+        
+        StopAllCoroutines();
+        
+        Vector3 previousPos = _grid.transform.position;
+
+        _rotateAxis.transform.position = _playerTrm.position;
+        
+        _grid.transform.position = previousPos;
+        
+        isRotate = true;
+        
+        Physics2D.gravity = new Vector2(0, 0);
+        
+        StartCoroutine(MapRotateCoroutine(direction));
+    }
+
+    private IEnumerator MapRotateCoroutine(Quaternion direction)
+    {
+        Quaternion start = _rotateAxis.transform.rotation;
+        float current = 0;
+        float percent = 0;
+        
+        Quaternion target = start * direction;
+
+
+        while (percent < 1)
+        {
+            current += Time.deltaTime;
+            percent = current / _time;
+
+            _rotateAxis.transform.rotation = Quaternion.Lerp(start, target, percent);
+            yield return null;
+        } 
+        
+
+        Physics2D.gravity = new Vector2(0, -9.81f);
+        isRotate = false;
+        _canRotate = false;
+        
+    }
+
+    public void Interact()
+    {
+        Debug.Log(transform.localEulerAngles.z);
+        Debug.Log(RotateManager.Instance.CurrentRotationIdx);
+        Debug.Log(RotateManager.Instance._rotations1[RotateManager.Instance.CurrentRotationIdx]);
+        if (transform.localEulerAngles.z ==
+            RotateManager.Instance._rotations1[RotateManager.Instance.CurrentRotationIdx])
+        {
+            _canRotate = true;
+            _showDirection.SetActive(true);
+        }
+        
+    }
+
+    public void EndInteract()
+    {
+        _canRotate = false;
+        _showDirection.SetActive(false);
+    }
+
+    public void Initialize(Transform playerTrm, Transform rotateAxis, GameObject grid, float time)
+    {
+        _playerTrm = playerTrm;
+        _rotateAxis = rotateAxis;
+        _grid = grid;
+        _time = time;
+    }
+}
