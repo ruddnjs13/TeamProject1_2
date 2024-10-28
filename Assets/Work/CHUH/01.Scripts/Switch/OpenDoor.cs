c9cb5b36d02b1f7e7068ecf851ae3b2a911c0aff
chuh007 <chhuh007@gmail.com>
2024-10-28 23:31:42 +0900

    fix LightShooter. Mirror

--- Assets/Work/CHUH/01.Scripts/Switch/OpenDoor.cs
+++ Assets/Work/CHUH/01.Scripts/Switch/OpenDoor.cs
@@ -3,25 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class OpenDoor : MonoBehaviour, ISwitchble
-//{
-//    private SpriteRenderer spriteRenderer;
-//    [SerializeField] private float moveRange = 5f;
-//    [SerializeField] private float duration = 1f;
-//    private void Awake()
-//    {
-//        spriteRenderer = GetComponent<SpriteRenderer>();
-//    }
-//    public void SwitchOnoff(bool onoff)
-//    {
-//        if(onoff == true)
-//        {
-//            Debug.Log("ÀÛµ¿");
-//            transform.DOLocalMove(transform.position + transform.up * moveRange, duration);
-//        }
-//        else
-//        {
-//            Debug.Log("²¨Áö");
-//        }
-//    }
-//}
+public class OpenDoor : MonoBehaviour, ISwitchble
+{
+    private SpriteRenderer spriteRenderer;
+    [SerializeField] private float moveRange = 5f;
+    [SerializeField] private float duration = 1f;
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    public void SwitchOnoff(bool onoff)
+    {
+        if (onoff == true)
+        {
+            Debug.Log("ÀÛµ¿");
+            transform.DOLocalMove(transform.position + transform.up * moveRange, duration);
+        }
+        else
+        {
+            Debug.Log("²¨Áö");
+        }
+    }
+}
