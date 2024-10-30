b6871b6996a8621af25af2f0331326342940c7c2
chuh007 <chhuh007@gmail.com>
2024-10-31 01:52:02 +0900

    fix

---
+++ Assets/Work/CHUH/01.Scripts/Switch/Multiplexer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Multiplexer : MonoBehaviour, ISwitchable
+{
+    [SerializeField] private List<ISwitchable> SwitchList = new List<ISwitchable>();
+    [SerializeField] private int maxActiveCount;
+    [SerializeField] private int nowActiveCount;
+    public UnityEvent ActiveObj;
+    public void Activate()
+    {
+        nowActiveCount++;
+        if(nowActiveCount >= maxActiveCount)
+        {
+            ActiveObj?.Invoke();
+        }
+    }
+
+    public void Deactivate()
+    {
+        nowActiveCount--;
+    }
+}
