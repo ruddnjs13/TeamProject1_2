b5866ccb8bd2c055a42ec72e71e84ac8ddc02f92
ruddnjs13 <ruddnjs013@gmail.com>
2024-11-04 00:18:46 +0900

    fix RotateManager

--- Assets/Work/LKW/01.Scripts/Gimmick/CheckPoint.cs
+++ Assets/Work/LKW/01.Scripts/Gimmick/CheckPoint.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CheckPoint : MonoBehaviour,IInteractable
 {
+    public int rotateIdx = 0;
+
+    private void Awake()
+    {
+        rotateIdx = (int)transform.rotation.eulerAngles.z / 90;
+    }
+
     public void StartInteract()
     {
         
