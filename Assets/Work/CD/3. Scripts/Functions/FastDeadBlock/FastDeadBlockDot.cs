f2141e22104fbe62a38c93121b3ba36699f96524
Candy <dltkr0424@gmail.com>
2024-11-04 00:50:49 +0900

    Fix : FastDeadBlockDot

--- Assets/Work/CD/3. Scripts/Functions/FastDeadBlock/FastDeadBlockDot.cs
+++ Assets/Work/CD/3. Scripts/Functions/FastDeadBlock/FastDeadBlockDot.cs
@@ -16,7 +16,7 @@ public class FastDeadBlockDot : MonoBehaviour
 
     private Tween _tween, _tween2;
     private Sequence _sequence;
-    private float _pos;
+    private Transform _pos;
     
     public void Start()
     {
@@ -25,24 +25,25 @@ public class FastDeadBlockDot : MonoBehaviour
 
     public void StopMove()
     {
-        _sequence.Pause();
+        Debug.Log("Invoke");
+        _tween.Kill();
+        _sequence.Kill();
     }
     
     public void SetSequence()
     {
-        _pos = transform.localPosition.x;
-        _tween = transform.DOMove(new Vector2(_pos, _position), _duration).SetEase(Ease.OutQuart);
+        _tween = transform.DOMove(new Vector2(transform.position.x, _position), _duration).SetEase(Ease.OutQuart);
 
         _sequence = DOTween.Sequence();
         _sequence.AppendInterval(_delayUpSecond)
             .Append(_tween)
             .AppendInterval(_delayUpSecond);
-        _sequence.SetLoops(-1, LoopType.Yoyo);
+        _sequence.SetLoops(-1, LoopType.Restart);
     }
 
-    public void EndMove()
+    public void StartMove()
     {
-        _sequence.Play();
+        SetSequence();
     }
 
     private void OnCollisionEnter2D(Collision2D other)
