acb2f688117b40bcb3d416e2d514e118c976e116
ruddnjs13 <ruddnjs013@gmail.com>
2024-11-20 21:49:30 +0900

    feat : AudioMixer  fix : CheckPoint

--- Assets/Work/LKW/01.Scripts/Initializable/BoxObject.cs
+++ Assets/Work/LKW/01.Scripts/Initializable/BoxObject.cs
@@ -7,8 +7,6 @@ public class BoxObject : InitializeObject
     protected override void Awake()
     {
         base.Awake();
-        Debug.Log(_originLocalPos);
-        Debug.Log(_originLocalRot);
     }
 
     public override void Initialize()
