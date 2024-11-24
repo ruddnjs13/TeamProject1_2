49ce39cb27eb0a7384648fb56751d510016b032b
chuh007 <chhuh007@gmail.com>
2024-11-24 12:54:57 +0900

    fix : Map1, SaveSystem, feat FruitSystem

---
+++ Assets/Work/CHUH/01.Scripts/Fruit/FruitProduction.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FruitProduction : MonoBehaviour
+{
+    [SerializeField] private Image fruitImage;
+    [SerializeField] private Image backPanel;
+    [SerializeField] private TextMeshProUGUI _nameText;
+    [SerializeField] private TextMeshProUGUI _TMIText;
+
+    private void Start()
+    {
+        
+    }
+
+    public void EatFruit()
+    {
+
+    }
+}
--- Assets/Work/CHUH/01.Scripts/Save/ScreenScaleSaveLoad.cs
+++ Assets/Work/CHUH/01.Scripts/Save/ScreenScaleSaveLoad.cs
@@ -7,18 +7,28 @@ public class ScreenScaleSaveLoad : MonoBehaviour
 {
     public ScreenScaleData ScreenScaleData;
 
+    private string path = Path.Combine(Application.dataPath, "ScreenScaleData.json");
+
+    private void Awake()
+    {
+        if(!File.Exists(path))
+        {
+            Debug.Log("파이리없는");
+            string jsonData = JsonUtility.ToJson(ScreenScaleData, true);
+            File.WriteAllText(path, jsonData);
+        }
+    }
+
     // 저장할때 호출
     public void SaveKeyDataToJson()
     {
         string jsonData = JsonUtility.ToJson(ScreenScaleData,true);
-        string path = Path.Combine(Application.dataPath, "ScreenScaleData.json");
         File.WriteAllText(path, jsonData);
     }
 
     // awake든 start든 이거 호출해서 값 복구
     public void LoadKeyDataFromJson()
     {
-        string path = Path.Combine(Application.dataPath, "ScreenScaleData.json");
         string jsonData = File.ReadAllText(path);
         ScreenScaleData = JsonUtility.FromJson<ScreenScaleData>(jsonData);
     }
