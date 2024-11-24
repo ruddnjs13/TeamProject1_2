fd12943ae0f26862d1c3c1d96f974c460aba1de6
ruddnjs13 <ruddnjs013@gmail.com>
2024-11-24 23:11:46 +0900

    faet : StartTimelineEvent

--- Assets/Work/CHUH/01.Scripts/Fruit/FruitProduction.cs
+++ Assets/Work/CHUH/01.Scripts/Fruit/FruitProduction.cs
@@ -4,9 +4,12 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.Events;
 
 public class FruitProduction : MonoBehaviour
 {
+    public UnityEvent EndEvent;
+
     [SerializeField] private Image fruitImage;
     [SerializeField] private Image backPanel;
     [SerializeField] private TextMeshProUGUI _nameText;
@@ -35,6 +38,9 @@ public class FruitProduction : MonoBehaviour
         text.maxVisibleCharacters = 0;
         _TMIText.enabled = true;
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(DOTween.To(x => text.maxVisibleCharacters = (int)x, 0f, text.text.Length, duration).SetEase(Ease.Linear));
+        sequence.Append(DOTween.To(x => text.maxVisibleCharacters = (int)x
+            , 0f, text.text.Length, duration).SetEase(Ease.Linear));
+
+        sequence.OnComplete(() => EndEvent?.Invoke());
     }
 }
